f98b87e7d0c16b9bea4db46e97f935cc1daa0f42
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-21 20:48:08 -0500

    completed exercise

--- wyswyg.js
+++ wyswyg.js
@@ -48,21 +48,64 @@ var userOutput = document.getElementById("user-output");
 
 function loopPeople(){
   userOutput.innerHTML = "";
-  console.log(people);
+  // console.log(people);
   for(var i=0; i<people.length;i++){
     //add card
     var newCard = "";
-    newCard += `<person class="card">`;
-    newCard += `<header class="card-header">${people[i].title}</header>`;
-    newCard += `<article class="image"><img src="${people[i].image}">`;
-    newCard += `<section class="section">${people[i].name}<br />${people[i].bio}</setion>`;
+    newCard += `<div class="card" id="card-${[i]}">`;
+    newCard += `<header class="card-header child">${people[i].title}</header>`;
+    newCard += `<article class="image child"><img class="grand-child" src="${people[i].image}">`;
+    newCard += `<section class="section grand-child">${people[i].name}<br /><p class="great-grand-child">${people[i].bio}</p></setion>`;
     // newCard += `<footer class="card-footer">${people[i].lifespan}</footer>`;
     newCard += `</article>`;
-    newCard += `</person>`;
+    newCard += `</div>`;
     userOutput.innerHTML += newCard;
   }
 }
 
 loopPeople();
+
+function addBorder(e){
+  removeBorder();
+  if(e.target.classList.contains("card")){
+    selectedBio = e.target.childNodes[1].childNodes[1].childNodes[2];
+    // e.target.childNodes[2].innerHTML = userInput;
+    // console.log("main card", e.target.childNodes);
+    e.target.classList.toggle("yellow-border");
+  }else if(e.target.classList.contains("child")){
+    e.target.parentNode.classList.toggle("yellow-border");
+    selectedBio = e.target.parentNode.childNodes[1].childNodes[1].childNodes[2];
+    // console.log("child", e.target.childNodes)
+  }else if(e.target.classList.contains("grand-child")){
+    selectedBio = e.target.parentNode.parentNode.childNodes[1].childNodes[1].childNodes[2];
+    e.target.parentNode.parentNode.classList.toggle("yellow-border");
+    // getting Bio
+    // selectedBio = e.target.childNodes[2];
+  }else if(e.target.classList.contains("great-grand-child")){
+    e.target.parentNode.parentNode.parentNode.classList.toggle("yellow-border");
+    selectedBio = e.target;
+  }
+  userInput.focus();
+}
+var selectedBio;
+
+function mirrorText(){
+  // userInput.value = selectedBio.innerHTML;
+  selectedBio.innerHTML = userInput.value;
+  // console.log("BIO",selectedBio);
+
+}
+
+function removeBorder(e){
+  var cardArray = document.getElementsByClassName("card");
+  for(var i=0;i<cardArray.length;i++){
+    console.log(cardArray[i]);
+    cardArray[i].classList.remove("yellow-border");
+  }
+}
+
+userInput.addEventListener("keyup", mirrorText);
+
+document.body.addEventListener("click", addBorder);
 
 // document.body.addEventListener("mouseenter", loopPeople);
